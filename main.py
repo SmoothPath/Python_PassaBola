c96fa32fa89f788bcacc70f56e471c618c1be7ea
Geovana <geovanamscardoso183@gmail.com>
2025-09-08 22:34:57 -0300

    Criar funções que validam email e senha

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#Importando bibliotecas
+import re
+
+
 def mostra_menu() -> None:
     """
     Essa função exibe um menu com as funcionalidades do sistema
@@ -14,3 +18,34 @@ def mostra_menu() -> None:
     print("[5] Ver Perfil")
     print("---------------------------------")
    
+def digita_email() -> str:
+    """
+    Essa função solicita que o usuário digite um e-mail,
+    valida esse e-mail e retorna ele em string
+    """
+    email: str = input("Digite o seu e-mail: ")
+
+    while not email.find("@") or not email.endswith(".com"):
+        email: str = input("E-mail inválido. Digite o e-mail novamente: ")
+
+    return email
+
+
+def digita_senha() -> str:
+    """
+    Essa função solicita que o usuário digite uma senha,
+    valida ela, e retorna ela em string
+    """
+    senha: str = input("Digite a sua senha: ")
+
+    while not (len(senha) >= 8 and
+           re.search(r"[A-Z]", senha) and
+           re.search(r"[a-z]", senha) and
+           re.search(r"[0-9]", senha) and
+           re.search(r"[\W_]", senha)):
+
+        senha: str = input("A senha deve ter pelo menos 8 caracteres, um número, uma letra maiúscula e minúscula, e um simbolo. Digite a senha novamente: ")
+
+
+    return senha
+
