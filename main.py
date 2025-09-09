5c0b0a742652caa08a6e38edc569a19154d42533
Geovana <geovanamscardoso183@gmail.com>
2025-09-08 23:05:15 -0300

    Criar funções para sair da conta, ver perfil e chamar funções

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def mostra_menu() -> None:
     print("---------------------------------")
     print("[1] Cadastrar Usuário")
     print("[2] Fazer Login")
-    print("[3] Consultar Eventos")
+    print("[3] Ver Perfil")
     print("[4] Sair da Conta")
-    print("[5] Ver Perfil")
+    print("[5] Consultar Eventos")
     print("---------------------------------")
    
 def digita_email() -> str:
@@ -103,8 +103,42 @@ def fazer_login() -> None:
 
     print("Você já está logado.\n")
 
+def sair_da_conta() -> None:
+    """
+    Essa função muda a variável do usuário logado,
+    fazendo um logout
+    """
+    global usuario_logado
+    if usuario_logado:
+        usuario_logado = None
+        print("Você saiu da conta.\n")
+    else:
+        print("Você não está logado.\n")
+
+def ver_perfil() -> None:
+    """
+    Essa função exibe os dados do usuário logado
+    """
+    if usuario_logado:
+        print("\n------ Perfil ------")
+        print(f"Nome: {usuario_logado[0]}")
+        print(f"E-mail: {usuario_logado[1]}")
+        print(f"Senha: {usuario_logado[2]}")
+    else:
+        print("Você precisa estar logado para ver o perfil.\n")
 
 
-        
+while True:
+    mostra_menu()
+    numero_menu = int(input("Digite a ação que deseja realizar: "))
+
+    if numero_menu == 1:
+        cadastra_usuario()
+    elif numero_menu == 2:
+        fazer_login()
+    elif numero_menu == 3:
+        ver_perfil() 
+    elif numero_menu == 4:
+        sair_da_conta()       
                
 
