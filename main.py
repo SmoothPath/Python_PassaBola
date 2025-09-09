91940d2f362e1548188e45f7696517959eae5db8
Geovana <geovanamscardoso183@gmail.com>
2025-09-08 22:30:05 -0300

    Criar menu base do sistema

---
+++ main.py
@@ -0,0 +1,16 @@
+def mostra_menu() -> None:
+    """
+    Essa função exibe um menu com as funcionalidades do sistema
+    """
+    print("---------------------------------")
+    print("| BEM VINDO(A) AO PASSA A BOLA! |")
+    print("|                               |")
+    print("| O que você deseja acessar?    |")
+    print("---------------------------------")
+    print("[1] Cadastrar Usuário")
+    print("[2] Fazer Login")
+    print("[3] Consultar Eventos")
+    print("[4] Sair da Conta")
+    print("[5] Ver Perfil")
+    print("---------------------------------")
+   
